8e391755ce65d527654fc2f12cdc9b2d65d9ee80
Chad Bentz <1760475+felickz@users.noreply.github.com>
2023-06-13 10:56:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, request, redirect, url_for, render_template_string
 # import jinja2, re, hashlib
 
 app = Flask(__name__)
-app.secret_key = 'SECRET_KEY'
+# not a real secret - simulation:
+app.secret_key = '11AANNZWY0bh0jIt1wgWSt_oZ8HWaNPgDEPWv1OsKSYfnBv3pw9PQsxr9D4OTseTZfFTFHXVJGPGdZx7h' 
 
 @app.route('/')
 def index():
